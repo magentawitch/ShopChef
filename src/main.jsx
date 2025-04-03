464b65b9679ddfdae81caf709e9fad976d2d33c9
Melissa Kuris <melissakuris@hotmail.com>
2025-04-03 17:23:18 -0300

    Added Context to Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RecipeProvider } from './Context/RecipeContext';
+import App from './App';
 import "./reset.css";
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  </StrictMode>,
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
+  </StrictMode>
 )
