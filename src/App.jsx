894899e3b9074220bc35d5be488ae497bfa75737
Melissa Kuris <melissakuris@hotmail.com>
2025-04-07 10:17:23 -0300

    Updated RecipeContex & App
    Added new functions to the context and set it in the provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./Components/Header/Header";
+import RecipeForm from "./Components/RecipeForm/RecipeForm";
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <RecipeForm />
     </>
   )
 }
--- src/Context/RecipeContext.jsx
+++ src/Context/RecipeContext.jsx
@@ -2,20 +2,31 @@ import { createContext, useState } from "react";
 
 const RecipeContext = createContext();
 
-export const RecipeProvider = ({children}) => {
+export const RecipeProvider = ({ children }) => {
     const [recipes, setRecipes] = useState([]);
     const [selectedRecipes, setSelectedRecipes] = useState([]);
+    const [ingredientsList, setIngredientsList] = useState([]);
 
-    const addRecipe = (title, ingredients) => {
-        setRecipes([...recipes, {title, ingredients}])
+
+    const addRecipe = (title, ingredients, instructions) => {
+        setRecipes([...recipes, { title: title, ingredients: ingredients, instructions: instructions }]);
+        setIngredientsList([]);
     }
 
+    const addIngredient = (ingredient) => {
+        setIngredientsList((prev) => [...prev, ingredient]);
+    }
+
+    const removeIngredient = (indexToRemove) => {
+        setIngredientsList((prev) => prev.filter((_, i) => i !== indexToRemove));
+    };
+
     return (
-        <RecipeContext.Provider value={{ recipes, addRecipe }}>
-          {children}
+        <RecipeContext.Provider value={{ recipes, addRecipe, ingredientsList, addIngredient, removeIngredient }}>
+            {children}
         </RecipeContext.Provider>
-      );
-      
+    );
+
 }
 
 export default RecipeContext;
