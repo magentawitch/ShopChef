326d1430c74075f77334080b1bbf043a0f126605
Melissa Kuris <melissakuris@hotmail.com>
2025-04-07 12:37:52 -0300

    Update Ingredient.jsx
    Added the amount and unit of ingredients into the ingredient chips

--- src/Components/Ingredient/Ingredient.jsx
+++ src/Components/Ingredient/Ingredient.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import RecipeContext from "../../Context/RecipeContext";
 import styles from "./Ingredient.module.css";
 
-function Ingredient({ name, index }) {
+function Ingredient({ name, amount, unit, index }) {
     const { removeIngredient } = useContext(RecipeContext);
     const icon = "x";
 
@@ -12,7 +12,7 @@ function Ingredient({ name, index }) {
 
     return(
         <div className={styles.ingredientContainer}>
-            <p className={styles.ingredient}>{name}</p>
+            <p className={styles.ingredient}>{amount} {unit} {name}</p>
             <button className={styles.removeButton} onClick={handleClick}>{icon}</button>
         </div>
     )
