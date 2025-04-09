f8433a098d6fda14368c41b3e1c341ac5baa41d2
Melissa Kuris <melissakuris@hotmail.com>
2025-04-09 20:31:28 -0300

    Update RecipeTile.jsx
    Added icon as variable so it can be changed on the parent.
    Plus, added unique keys to the ingredients

--- src/Components/RecipeTile/RecipeTile.jsx
+++ src/Components/RecipeTile/RecipeTile.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import styles from "./RecipeTile.module.css";
 
-function RecipeTile({ title, ingredients }) {
+function RecipeTile({ title, ingredients, icon, onClick }) {
 
 
     return (
         <div className={styles.tileContainer}>
             <div className={styles.titleContainer}>
             <h3 className={styles.title}>{title}</h3>
+            <button className={styles.addButton} onClick={onClick}>{icon}</button>
             </div>
             <div className={styles.ingredientsContainer}>
-                {ingredients.map((ingredient) =>
-                    <p className={styles.ingredient}>{ingredient}</p>
+                {ingredients.map((ingredient, i) =>
+                    <p key={i} className={styles.ingredient}>{ingredient}</p>
                 )}
             </div>
 
