dc070b92f4b4d0be94fb4b16abe5e3bcb8a003b4
Melissa Kuris <melissakuris@hotmail.com>
2025-04-09 20:30:39 -0300

    Added functionality and style to RecipeList
    It now renders recipe tiles for each recipe on the recipes list. Plus it adds it to the selectedRecipes list.

--- src/Components/RecipeList/RecipeList.jsx
+++ src/Components/RecipeList/RecipeList.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import RecipeContext from "../../Context/RecipeContext";
+import RecipeTile from "../RecipeTile/RecipeTile";
+import styles from "./RecipeList.module.css";
 
 function RecipeList() {
+    const { recipes, selectRecipe } = useContext(RecipeContext);
 
+    const handleClick = index => {
+        selectRecipe(index);
+    }
+
+    return (
+        <div className={styles.recipesContainer}>
+            <div className={styles.recipe}>
+                {recipes.map((recipe, i) =>
+                    <RecipeTile
+                        key={i}
+                        icon="+"
+                        title={recipe.title}
+                        ingredients={recipe.ingredients}
+                        onClick={() =>{handleClick(i)}}
+                    />
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default RecipeList;
