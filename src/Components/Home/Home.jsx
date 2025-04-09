857cbd6f40d35e15f4c55c00cbd32fba16040898
Melissa Kuris <melissakuris@hotmail.com>
2025-04-09 19:10:18 -0300

    Created Home Component and its style Module

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Home.module.css";
+import RecipeList from "../RecipeList/RecipeList";
+
+function Home() {
+    return (
+        <>
+        <RecipeList />
+        </>  
+    )
+}
+
+export default Home;
