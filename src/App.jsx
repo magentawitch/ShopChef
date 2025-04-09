6221361c3fa2dcb3f9e33c94b447e0e1d113102d
Melissa Kuris <melissakuris@hotmail.com>
2025-04-09 16:23:52 -0300

    Added Routing to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import Header from "./Components/Header/Header";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import Root from "./Components/Root/Root.jsx";
 import RecipeForm from "./Components/RecipeForm/RecipeForm";
+import RecipeList from "./Components/RecipeList/RecipeList.jsx";
 
+
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={ <Root /> }>
+    <Route index element={ <RecipeForm /> } />
+    <Route path="/recipes" element={ <RecipeList />} />  
+  </Route>
+));
 
 function App() {
 
   return (
-    <>
-      <Header />
-      <RecipeForm />
-    </>
+    <RouterProvider router={appRouter} />
   )
 }
 
