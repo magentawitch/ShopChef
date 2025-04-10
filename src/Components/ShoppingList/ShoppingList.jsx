a846a52745b995b2dc9af7df12d9e1cc962504ab
Melissa Kuris <melissakuris@hotmail.com>
2025-04-10 13:40:33 -0300

    Update ShoppingList.jsx
    Added the logic to include and calculate the items in the shopping list

--- src/Components/ShoppingList/ShoppingList.jsx
+++ src/Components/ShoppingList/ShoppingList.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import RecipeContext from "../../Context/RecipeContext";
+import styles from "./ShoppingList.module.css";
 
 function ShoppingList() {
+    const { selectedRecipes } = useContext(RecipeContext);
+    const [ingredientsToBuy, setIngredientsToBuy] = useState([]);
 
+    useEffect(() => {
+        updateShoppingList();
+    }, [selectedRecipes])
+
+
+    const updateShoppingList = () => {
+        let updatedList = [];
+
+        selectedRecipes.forEach(recipe => {
+            recipe.ingredients.forEach(ingredient => {
+                const existing = updatedList.find(item =>
+                    item.name === ingredient.name && item.unit === ingredient.unit
+                );
+
+                if (existing) {
+                    existing.amount += Number(ingredient.amount);
+                } else {
+                    updatedList.push({
+                        name: ingredient.name,
+                        unit: ingredient.unit,
+                        amount: Number(ingredient.amount)
+                    })
+                }
+            })
+        })
+
+        setIngredientsToBuy(updatedList);
+    }
+
+
+    return (
+        <div className={styles.ingredientListContainer}>
+            <div className={styles.ingredientItemContainer}>
+                {ingredientsToBuy.length === 0 ? (
+                    <p className={styles.emptyText}>Your shopping list is empty ✨</p>
+                ) : (
+                    <ul>
+                        {ingredientsToBuy.map((ingredient, i) => (
+                            <li key={i} className={styles.ingredient}>
+                                <label className={styles.customCheckbox}>
+                                    <input
+                                        type="checkbox"
+                                        className={styles.hiddenCheckbox}
+                                    />
+                                    <span className={styles.styledCheckbox}></span>
+                                    {ingredient.amount} {ingredient.unit} {ingredient.name}
+                                </label>
+                            </li>
+                        ))}
+                    </ul>)}
+            </div>
+        </div>
+    )
 }
 
 export default ShoppingList;
+
